1b55f915dd881c525ae27cf1ad696966d637f10e
BrookeBryski <brookeabryski@gmail.com>
2023-06-21 16:20:54 -0700

    added routes with queries and params

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ app.get('/', (req, res) => {
     res.send("home")
 })
 
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.send(`<h1>welcome to r/${subreddit}</h1>`)
+})
+
+app.get('/r/:subreddit/:postId', (req, res) => {
+    const { subreddit, postId } = req.params;
+    res.send(`<h1> viewing the post ${postId} on r/${subreddit}</h1>`)
+})
+
+app.get('/search', (req, res) => {
+    const { q } = req.query;
+    if (!q) {
+        res.send('Cannot send search results with no search.')
+    }
+        res.send(`<h1>Search results for ${q}</h1>`)
+})
+
 app.get('/cats', (req, res) => {
     res.send("meow")
 })
